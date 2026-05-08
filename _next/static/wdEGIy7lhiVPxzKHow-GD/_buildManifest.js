b8f18ae81174be5cc5d97d9012920c3c284bebc6
pratik0-code <programmingguruji3@gmail.com>
2026-05-08 22:23:45 +0545

    Updates

--- _next/static/wdEGIy7lhiVPxzKHow-GD/_buildManifest.js
+++ _next/static/wdEGIy7lhiVPxzKHow-GD/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/fuyalpratik-portfolio//_next/:path+",
-        "destination": "/fuyalpratik-portfolio/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
